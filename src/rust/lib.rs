07836e7ba2a4ccc6c7db384f03c0aaf8cb656db9
vinod reddy gopi reddy <vinodreddy.gopireddy@qorix.ai>
2025-06-24 13:16:42 +0200

    kvs: Kvs as generic
    - Added kvstype as default for builder
    - Added kvstrait as a type

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -226,7 +226,7 @@ pub enum ErrorCode {
 }
 
 /// Key-value-storage builder
-pub struct KvsBuilder {
+pub struct KvsBuilder<T=Kvs> {
     /// Instance ID
     instance_id: InstanceId,
 
@@ -235,6 +235,7 @@ pub struct KvsBuilder {
 
     /// Need-KVS flag
     need_kvs: bool,
+    _phantom: std::marker::PhantomData<T>,
 }
 
 /// Key-value-storage data
@@ -439,7 +440,41 @@ impl SnapshotId {
     }
 }
 
-impl KvsBuilder {
+pub trait KvsApi {
+    fn open(
+        instance_id: InstanceId,
+        need_defaults: OpenNeedDefaults,
+        need_kvs: OpenNeedKvs,
+    ) -> Result<Self, ErrorCode>
+    where
+        Self: Sized;
+
+    fn reset(&self) -> Result<(), ErrorCode>;
+    fn get_all_keys(&self) -> Result<Vec<String>, ErrorCode>;
+    fn key_exists(&self, key: &str) -> Result<bool, ErrorCode>;
+    fn get_value<T>(&self, key: &str) -> Result<T, ErrorCode>
+    where
+        for<'a> T: TryFrom<&'a KvsValue> + Clone,
+        for<'a> <T as TryFrom<&'a KvsValue>>::Error: std::fmt::Debug;
+    fn get_default_value(&self, key: &str) -> Result<KvsValue, ErrorCode>;
+    fn is_value_default(&self, key: &str) -> Result<bool, ErrorCode>;
+    fn set_value<S: Into<String>, J: Into<KvsValue>>(
+        &self,
+        key: S,
+        value: J,
+    ) -> Result<(), ErrorCode>;
+    fn remove_key(&self, key: &str) -> Result<(), ErrorCode>;
+    fn flush(&self) -> Result<(), ErrorCode>;
+    fn snapshot_count(&self) -> usize;
+    fn snapshot_max_count() -> usize
+    where
+        Self: Sized;
+    fn snapshot_restore(&self, id: SnapshotId) -> Result<(), ErrorCode>;
+    fn get_kvs_filename(&self, id: SnapshotId) -> String;
+    fn get_hash_filename(&self, id: SnapshotId) -> String;
+}
+
+impl<T> KvsBuilder<T> where T: KvsApi {
     /// Create a builder to open the key-value-storage
     ///
     /// Only the instance ID must be set. All other settings are using default values until changed
@@ -455,6 +490,7 @@ impl KvsBuilder {
             instance_id,
             need_defaults: false,
             need_kvs: false,
+            _phantom: std::marker::PhantomData,
         }
     }
 
@@ -465,7 +501,7 @@ impl KvsBuilder {
     ///
     /// # Return Values
     ///   * KvsBuilder instance
-    pub fn need_defaults(mut self, flag: bool) -> KvsBuilder {
+    pub fn need_defaults(mut self, flag: bool) -> KvsBuilder<T> {
         self.need_defaults = flag;
         self
     }
@@ -477,7 +513,7 @@ impl KvsBuilder {
     ///
     /// # Return Values
     ///   * KvsBuilder instance
-    pub fn need_kvs(mut self, flag: bool) -> KvsBuilder {
+    pub fn need_kvs(mut self, flag: bool) -> KvsBuilder<T> {
         self.need_kvs = flag;
         self
     }
@@ -498,8 +534,8 @@ impl KvsBuilder {
     ///   * `ErrorCode::KvsFileReadError`: KVS file read error
     ///   * `ErrorCode::KvsHashFileReadError`: KVS hash file read error
     ///   * `ErrorCode::UnmappedError`: Generic error
-    pub fn build(self) -> Result<Kvs, ErrorCode> {
-        Kvs::open(
+    pub fn build(self) -> Result<T, ErrorCode> {
+        T::open(
             self.instance_id,
             self.need_defaults.into(),
             self.need_kvs.into(),
